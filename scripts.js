815f3363526320527e4e9fe770ee1a64ec2d7c19
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-11-29 13:03:28 -0700

    Script Changes Added Css classes for matched status Rewrote password validation script Added better names for password variables Added Message to show passwords aren't matching

--- scripts.js
+++ scripts.js
@@ -1,16 +1,34 @@
 const password1 = document.querySelector("#password");
 const password2 = document.querySelector('#confirm-password');
+const passwordMessage = document.querySelector("#password-message");
 
 
-function isMatch(p1, p2) {
-    if (p1.value !== p2.value) {
-        p2.style.border = "2px solid red"
-        p1.style.border = "2px solid red"
+function isMatch(password1, password2) {
+
+    if (password1.value === password2.value) {
+        if (password1.value === '') {
+            password2.className = '';
+            password1.className = '';
+            passwordMessage.textContent = ""
+            return
+        }
+        password2.className = '';
+        password1.className = '';
+        password2.classList.add('matched')
+        password1.classList.add('matched')
+        passwordMessage.textContent = ""
+
     } else {
-        p2.style.border = "2px solid green"
-        p1.style.border = "2px solid green"
+        password2.className = '';
+        password1.className = '';
+        password2.classList.add('notMatched')
+        password1.classList.add('notMatched')
+        passwordMessage.textContent = "* Passwords do not match"
+
+
     }
 
 }
 
 password2.addEventListener('keyup', (event) => isMatch(password1, password2));
+password1.addEventListener('keyup', (event) => isMatch(password1, password2));
